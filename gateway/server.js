30dd3143b4bb5f11f21e7b594a619852f1047739
DESKTOP-E7J29JO\duykh <duykhoi.bui96@gmail.com>
2018-04-19 15:48:46 +0700

    Sua loi CORS tren GATEWAY

--- gateway/server.js
+++ gateway/server.js
@@ -18,6 +18,7 @@ var server = require('http').Server(app);
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
 
